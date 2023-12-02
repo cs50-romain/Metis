8a87aa872ac598d4dea98551d2e3eb1248481915
cs50-romain <cs50.git.romain@gmail.com>
2023-12-02 00:07:44 -0500

    Started golang server

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
 
 func main() {
 	fmt.Println("Hello")
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	log.Print(http.ListenAndServe(":8080", nil))
 }
