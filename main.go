5cebbc33dc3db1079c05c445c72d516bcd29aafc
cs50-romain <cs50.git.romain@gmail.com>
2023-12-04 22:57:47 -0500

    Started keyboard shortcuts feature

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ var itasks []ImportantTask
 var mtasks []MinorTask
 var ltasks []LaterTask
 
+const PATH_SEP_WINDOWS = '\\'
+const PATHH_SEP_LINUX = '/'	
+
 type Tasks struct {
 	Itasks	[]ImportantTask
 	Mtasks	[]MinorTask
@@ -25,6 +28,7 @@ type ImportantTask struct {
 	Id		int
 	Content		string
 	isCompleted	bool
+	CreatedAt	time.Time
 }
 
 type MinorTask struct {
@@ -56,12 +60,13 @@ func addImportantItem(w http.ResponseWriter, r *http.Request) {
 		content := r.PostFormValue("content")
 		newid := id
 		id++
-		if empty(content) {
-			return
-		}
 
+		if empty(content) {
+
+		}
+			
 		if len(itasks) <= 0 {
-			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nImportant\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-important-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form hx-post='/add-important-item/' hx-target='#task-important-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
+			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nImportant\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-important-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form hx-post='/add-important-item/' hx-target='#task-important-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' id='important-input' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
 
 			tmpl, _ := template.New("t").Parse(htmlEl)
 			tmpl.Execute(w, nil)
@@ -71,7 +76,7 @@ func addImportantItem(w http.ResponseWriter, r *http.Request) {
 			tmpl.Execute(w, nil)
 		}
 
-		itasks = append(itasks, ImportantTask{newid, content, false})
+		itasks = append(itasks, ImportantTask{newid, content, false, time.Now()})
 	}
 }
 
@@ -83,8 +88,12 @@ func addMinorItem(w http.ResponseWriter, r *http.Request) {
 		newid := id
 		id++
 
+		if empty(content) {
+
+		}
+
 		if len(mtasks) <= 0 {
-			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nMinor\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-minor-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form hx-post='/add-minor-item/' hx-target='#task-minor-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
+			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nMinor\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-minor-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form hx-post='/add-minor-item/' hx-target='#task-minor-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' id='minor-input' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
 
 			tmpl, _ := template.New("t").Parse(htmlEl)
 			tmpl.Execute(w, nil)
@@ -106,8 +115,12 @@ func addLaterItem(w http.ResponseWriter, r *http.Request) {
 		newid := id
 		id++
 
+		if empty(content) {
+
+		}
+
 		if len(ltasks) <= 0 {
-			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nFor Later\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-later-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form hx-post='/add-later-item/' hx-target='#task-later-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
+			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nFor Later\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-later-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form hx-post='/add-later-item/' hx-target='#task-later-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' id='later-input' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
 
 			tmpl, _ := template.New("t").Parse(htmlEl)
 			tmpl.Execute(w, nil)
@@ -149,7 +162,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func saveFile() {
 	for {
-		// Once session are created, check to see if the session is inactive, if so save the file and exit.
+		// Once sessions are created, check to see if the session is inactive, if so save the file and exit.
 		tasks := Tasks{
 			Itasks: itasks,
 			Mtasks: mtasks,
@@ -158,23 +171,45 @@ func saveFile() {
 
 		b, err := json.Marshal(tasks)
 		if err != nil {
-			log.Print("{!] Couldn't save file retrying...")
+			log.Print("{!] Couldn't save file, retrying in...")
 		} else {
 			fmt.Println("[+] File saved")
 		}
 
-		err = os.WriteFile("./data.json", b, 0644)
-		if err != nil {
-			log.Print("[!] Error saving to file")
+		if os.PathSeparator == PATH_SEP_WINDOWS {
+			err = os.WriteFile("C:\\Program Files\\Metis\\data.json", b, 0644)
+			if err != nil {
+				log.Print("[!] Error saving to file")
+			}
+		} else {
+			err = os.WriteFile("./data.json", b, 0644)
+			if err != nil {
+				log.Print("[!] Error saving to file")
+			}
 		}
 		time.Sleep(10000 * time.Millisecond)
 	}
 }
 
+func prefill() {
+	var tasks Tasks
+	b, err := os.ReadFile("./data.json")
+	err = json.Unmarshal(b, &tasks)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	itasks = tasks.Itasks
+	mtasks = tasks.Mtasks
+	ltasks = tasks.Ltasks
+}
+
 func main() {
 	id = 0
 
-	fmt.Println("[+] Starting server...")
+	fmt.Println("[+] Decoding json information if any...")
+	prefill()
+	fmt.Println("[+] Starting web server...")
 	go saveFile()
 	http.HandleFunc("/", index)
 	http.HandleFunc("/add-important-item/", addImportantItem)
@@ -186,7 +221,7 @@ func main() {
 /*
 TODO
 1. Add ability to search for a youtube video. Make window a little bit bigger and in the future resizable
-2. Save json to file: import and export the json data. - TODAY
+2. Save json to file: import and export the json data. - DONE 
 3. Session per client connection - TODAY
 4. Keyboard shortcuts
 5. Draggable task items (within flex and from one flex, like from Later section to Important to another)
