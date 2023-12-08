1118cd2e3cc83574bd0ad50b29afd37bc8931948
cs50-romain <cs50.git.romain@gmail.com>
2023-12-07 20:30:46 -0500

    Added own Router and its new routes; Deleted old routes

--- main.go
+++ main.go
@@ -42,105 +42,58 @@ func empty(content string) bool{
 	return false
 }
 
-func addImportantItem(w http.ResponseWriter, r *http.Request) {
-	log.Print("HTMX request received")
-	log.Print(r.Header.Get("HX-REQUEST"))
+func Route(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request received for %s; Routing...\n", r.URL.Path)
+	requestpath := strings.Split(r.URL.Path, "/")
+	urlpath := requestpath[1]
+	importance := requestpath[len(requestpath)-1]
+	
+	if r.URL.Path == "/" {
+		index(w, r)
+	} else if urlpath == "add-item" {
+		AddItem(w, r, importance)
+	} else if urlpath == "delete" {
+		DeleteItem(w, r)
+	} else {
+		log.Printf("Invalid Path Request: %s\n", r.URL.Path)
+		http.Error(w, "Invalid Path Request", http.StatusBadRequest)
+	}
+}
+
+func AddItem(w http.ResponseWriter, r *http.Request, importance string) {
 	if r.Method == "POST" {
 		content := r.PostFormValue("content")
 		newid := id
 		id++
 
 		if empty(content) {
-
-		}
-			
-		if len(itasks) <= 0 {
-			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nImportant\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-important-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n<button hx-delete='/delete/minor/%b' hx-target='closest div' hx-swap='innerhtml' type='button' class='text-white bg-blue-700 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-small rounded-full text-sm p-0.5 text-center inline-flex items-end me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-black-800'>\n<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24'><path fill='currentColor' d='M16 1.75V3h5.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75Zm-6.5 0V3h5V1.75a.25.25 0 0 0-.25-.25h-4.5a.25.25 0 0 0-.25.25ZM4.997 6.178a.75.75 0 1 0-1.493.144L4.916 20.92a1.75 1.75 0 0 0 1.742 1.58h10.684a1.75 1.75 0 0 0 1.742-1.581l1.413-14.597a.75.75 0 0 0-1.494-.144l-1.412 14.596a.25.25 0 0 1-.249.226H6.658a.25.25 0 0 1-.249-.226L4.997 6.178Z'/>\n<path fill='currentColor' d='M9.206 7.501a.75.75 0 0 1 .793.705l.5 8.5A.75.75 0 1 1 9 16.794l-.5-8.5a.75.75 0 0 1 .705-.793Zm6.293.793A.75.75 0 1 0 14 8.206l-.5 8.5a.75.75 0 0 0 1.498.088l.5-8.5Z'/>\n</svg>\n<span class='sr-only'>Icon description</span>\n</button>\n</div>\n</div>\n</div>\n<form id='minor-input-form' hx-post='/add-minor-item/' hx-target='#task-minor-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' id='minor-input' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content, newid)
-			tmpl, _ := template.New("t").Parse(htmlEl)
-			tmpl.Execute(w, nil)
-		} else {
-
-			htmlel := fmt.Sprintf("<div class='items-center bg-[#1da1f2] space-x-2 text-white rounded-lg mb-2' id='task-important-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 20 20' fill='currentcolor'>\n<path fill-rule='evenodd' d='m16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414l8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n<button hx-delete='/delete/important/%b' hx-target='closest div' hx-swap='innerhtml' type='button' class='text-white bg-blue-700 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-small rounded-full text-sm p-0.5 text-center inline-flex items-end me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-black-800'>\n<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24'><path fill='currentColor' d='M16 1.75V3h5.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75Zm-6.5 0V3h5V1.75a.25.25 0 0 0-.25-.25h-4.5a.25.25 0 0 0-.25.25ZM4.997 6.178a.75.75 0 1 0-1.493.144L4.916 20.92a1.75 1.75 0 0 0 1.742 1.58h10.684a1.75 1.75 0 0 0 1.742-1.581l1.413-14.597a.75.75 0 0 0-1.494-.144l-1.412 14.596a.25.25 0 0 1-.249.226H6.658a.25.25 0 0 1-.249-.226L4.997 6.178Z'/>\n<path fill='currentColor' d='M9.206 7.501a.75.75 0 0 1 .793.705l.5 8.5A.75.75 0 1 1 9 16.794l-.5-8.5a.75.75 0 0 1 .705-.793Zm6.293.793A.75.75 0 1 0 14 8.206l-.5 8.5a.75.75 0 0 0 1.498.088l.5-8.5Z'/>\n</svg>\n<span class='sr-only'>Icon description</span>\n</button>\n</div>\n</div>", newid, content, newid) 
-			tmpl, _ := template.New("t").Parse(htmlel)
-			tmpl.Execute(w, nil)
+			http.Error(w, "No content", http.StatusBadRequest)
 		}
 
-		itasks = append(itasks, Task{newid, content, false, time.Now(), "important"})
+		if importance == "important" {
+			itasks = append(itasks, Task{newid, content, false, time.Now(), "later"})
+		} else if importance == "minor" {
+			mtasks = append(mtasks, Task{newid, content, false, time.Now(), "later"})
+		} else if importance == "later" {
+			ltasks = append(ltasks, Task{newid, content, false, time.Now(), "later"})
+		}
+
+		htmlEl := fmt.Sprintf("<li id=%b class='todo-item'>\n<label>\n<input type='checkbox'>%s\n</label>\n<button>Delete</button>\n</li>", newid, content)
+
+		tmpl, _ := template.New("t").Parse(htmlEl)
+		tmpl.Execute(w, nil)
 		go saveFile()
 	}
 }
 
-func addMinorItem(w http.ResponseWriter, r *http.Request) {
-	log.Print("HTMX request received")
-	log.Print(r.Header.Get("HX-REQUEST"))
-	if r.Method == "POST" {
-		content := r.PostFormValue("content")
-		newid := id
-		id++
-
-		if empty(content) {
-
-		}
-
-		fmt.Println(len(mtasks))
-
-		// The whole form is being re-rendered after we delete that last element in the list. Thisgives the effect of 2 of the same thing appearing. I need a way that this does not happen. Ideally I don't need to check the length and I can just render the htmlel that's in the else statement.
-		if len(mtasks) <= 0 {
-			fmt.Println("We are here")
-			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nMinor\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-minor-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n<button hx-delete='/delete/minor/%b' hx-target='closest div' hx-swap='innerhtml' type='button' class='text-white bg-blue-700 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-small rounded-full text-sm p-0.5 text-center inline-flex items-end me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-black-800'>\n<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24'><path fill='currentColor' d='M16 1.75V3h5.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75Zm-6.5 0V3h5V1.75a.25.25 0 0 0-.25-.25h-4.5a.25.25 0 0 0-.25.25ZM4.997 6.178a.75.75 0 1 0-1.493.144L4.916 20.92a1.75 1.75 0 0 0 1.742 1.58h10.684a1.75 1.75 0 0 0 1.742-1.581l1.413-14.597a.75.75 0 0 0-1.494-.144l-1.412 14.596a.25.25 0 0 1-.249.226H6.658a.25.25 0 0 1-.249-.226L4.997 6.178Z'/>\n<path fill='currentColor' d='M9.206 7.501a.75.75 0 0 1 .793.705l.5 8.5A.75.75 0 1 1 9 16.794l-.5-8.5a.75.75 0 0 1 .705-.793Zm6.293.793A.75.75 0 1 0 14 8.206l-.5 8.5a.75.75 0 0 0 1.498.088l.5-8.5Z'/>\n</svg>\n<span class='sr-only'>Icon description</span>\n</button>\n</div>\n</div>\n</div>\n<form id='minor-input-form' hx-post='/add-minor-item/' hx-target='#task-minor-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' id='minor-input' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content, newid)
-
-			tmpl, _ := template.New("t").Parse(htmlEl)
-			tmpl.Execute(w, nil)
-		} else {
-
-			htmlel := fmt.Sprintf("<div class='items-center bg-[#1da1f2] space-x-2 text-white rounded-lg mb-2' id='task-minor-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 20 20' fill='currentcolor'>\n<path fill-rule='evenodd' d='m16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414l8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n<button hx-delete='/delete/minor/%b' hx-target='closest div' hx-swap='innerhtml' type='button' class='text-white bg-blue-700 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-small rounded-full text-sm p-0.5 text-center inline-flex items-end me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-black-800'>\n<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24'><path fill='currentColor' d='M16 1.75V3h5.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75Zm-6.5 0V3h5V1.75a.25.25 0 0 0-.25-.25h-4.5a.25.25 0 0 0-.25.25ZM4.997 6.178a.75.75 0 1 0-1.493.144L4.916 20.92a1.75 1.75 0 0 0 1.742 1.58h10.684a1.75 1.75 0 0 0 1.742-1.581l1.413-14.597a.75.75 0 0 0-1.494-.144l-1.412 14.596a.25.25 0 0 1-.249.226H6.658a.25.25 0 0 1-.249-.226L4.997 6.178Z'/>\n<path fill='currentColor' d='M9.206 7.501a.75.75 0 0 1 .793.705l.5 8.5A.75.75 0 1 1 9 16.794l-.5-8.5a.75.75 0 0 1 .705-.793Zm6.293.793A.75.75 0 1 0 14 8.206l-.5 8.5a.75.75 0 0 0 1.498.088l.5-8.5Z'/>\n</svg>\n<span class='sr-only'>Icon description</span>\n</button>\n</div>\n</div>", newid, content, newid) 
-
-			tmpl, _ := template.New("t").Parse(htmlel)
-			tmpl.Execute(w, nil)
-		}
-
-		mtasks = append(mtasks, Task{newid, content, false, time.Now(), "minor"})
-		go saveFile()
-	}
-}
-
-func addLaterItem(w http.ResponseWriter, r *http.Request) {
-	log.Print("HTMX request received")
-	log.Print(r.Header.Get("HX-REQUEST"))
-	if r.Method == "POST" {
-		content := r.PostFormValue("content")
-		newid := id
-		id++
-
-		if empty(content) {
-
-		}
-
-		if len(ltasks) <= 0 {
-			htmlEl := fmt.Sprintf("<div class='flex m-auto text-center text-2xl'>\nFor Later\n</div>\n<div class='flex flex-wrap space-x-2 flex-col p-4 w-2/6 h-32'>\n<div class='items-center bg-[#1da1f2] text-white rounded-lg mb-2' id='task-later-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org2000/svg' viewBox='0 0 20 20' fill='currentColor'>\n<path fill-rule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n</div>\n</div>\n</div>\n<form id='later-input-form' hx-post='/add-later-item/' hx-target='#task-later-list' hx-swap='afterend'>\n<div class='flex-none flex items-center border-b border-teal-500 py-2'>\n<input name='content' id='later-input' class='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' type='text' placeholder='Add Item'>\n<button class='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded' type='submit'>Add</button>\n</div>\n</form>\n", newid, content)
-
-			tmpl, _ := template.New("t").Parse(htmlEl)
-			tmpl.Execute(w, nil)
-		} else {
-			htmlel := fmt.Sprintf("<div class='items-center bg-[#1da1f2] space-x-2 text-white rounded-lg mb-2' id='task-later-list'>\n<div>\n<input class='hidden' type='checkbox' id=%b name='item' checked>\n<label class='flex items-center h-7 px-2 rounded cursor-pointer' for='task_1'>\n<span class='flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full'>\n<svg class='w-4 h-4 fill-current' xmlns='http://www.w3.org/2000/svg' viewbox='0 0 20 20' fill='currentcolor'>\n<path fill-rule='evenodd' d='m16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414l8 12.586l7.293-7.293a1 1 0 011.414 0z' clip-rule='evenodd' />\n</svg>\n</span>\n<span class='ml-4 text-sm'>%s</span>\n</label>\n<button hx-delete='/delete/later/%b' hx-target='closest div' hx-swap='innerhtml' type='button' class='text-white bg-blue-700 hover:bg-red-800 focus:ring-2 focus:outline-none focus:ring-red-300 font-small rounded-full text-sm p-0.5 text-center inline-flex items-end me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-black-800'>\n<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24'><path fill='currentColor' d='M16 1.75V3h5.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75Zm-6.5 0V3h5V1.75a.25.25 0 0 0-.25-.25h-4.5a.25.25 0 0 0-.25.25ZM4.997 6.178a.75.75 0 1 0-1.493.144L4.916 20.92a1.75 1.75 0 0 0 1.742 1.58h10.684a1.75 1.75 0 0 0 1.742-1.581l1.413-14.597a.75.75 0 0 0-1.494-.144l-1.412 14.596a.25.25 0 0 1-.249.226H6.658a.25.25 0 0 1-.249-.226L4.997 6.178Z'/>\n<path fill='currentColor' d='M9.206 7.501a.75.75 0 0 1 .793.705l.5 8.5A.75.75 0 1 1 9 16.794l-.5-8.5a.75.75 0 0 1 .705-.793Zm6.293.793A.75.75 0 1 0 14 8.206l-.5 8.5a.75.75 0 0 0 1.498.088l.5-8.5Z'/>\n</svg>\n<span class='sr-only'>Icon description</span>\n</button>\n</div>\n</div>", newid, content, newid) 
-
-			tmpl, _ := template.New("t").Parse(htmlel)
-			tmpl.Execute(w, nil)
-		}
-
-		ltasks = append(ltasks, Task{newid, content, false, time.Now(), "later"})
-		go saveFile()
-	}
-}
-
-func deleteItem(w http.ResponseWriter, r *http.Request) {
+func DeleteItem(w http.ResponseWriter, r *http.Request) {
 	log.Print("Delete request received")
 	path := strings.Split(r.URL.Path, "/")
 	idString := path[len(path)-1]
 	fmt.Println(idString)
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		log.Print("Line 135", err)
+		log.Print("String Conversion Error:", err)
 	}
 	importance := path[len(path)-2]
 
@@ -316,11 +269,8 @@ func main() {
 	prefill()
 	fmt.Println("[+] Starting web server...")
 
-	http.HandleFunc("/", index)
-	http.HandleFunc("/add-important-item/", addImportantItem)
-	http.HandleFunc("/add-minor-item/", addMinorItem)
-	http.HandleFunc("/add-later-item/", addLaterItem)
-	http.HandleFunc("/delete/", deleteItem)
+	http.HandleFunc("/", Route)
+
 	log.Print(http.ListenAndServe(":8080", nil))
 }
 
